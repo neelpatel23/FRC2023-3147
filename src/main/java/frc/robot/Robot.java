e54901d0ad2ac4b24c42e0989be31b6717946e3e
Neel Patel <neelp2023@gmail.com>
2023-01-22 21:33:43 -0600

    Version 1 (DriveTrain)

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,151 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+
+import java.util.concurrent.CancellationException;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+public class Robot extends TimedRobot {
+  private Command m_autonomousCommand;
+
+  private RobotContainer m_robotContainer;
+
+  private XboxController controller = new XboxController(0);
+
+  private CANSparkMax m_leftFront = new CANSparkMax(1, MotorType.kBrushless);
+  private CANSparkMax m_leftRear = new CANSparkMax(3, MotorType.kBrushless);
+  private CANSparkMax m_rightFront = new CANSparkMax(2, MotorType.kBrushless);
+  private CANSparkMax m_rightRear = new CANSparkMax(4, MotorType.kBrushless);
+
+  private MotorControllerGroup m_left = new MotorControllerGroup(m_leftFront, m_leftRear);
+  private MotorControllerGroup m_right = new MotorControllerGroup(m_rightFront, m_rightRear);
+
+  private double DriveXValue;
+  private double DriveYValue;
+
+  
+  
+  private DifferentialDrive m_drive = new DifferentialDrive(m_left, m_right);
+
+  /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
+  @Override
+  public void robotInit() {
+    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
+    // autonomous chooser on the dashboard.
+    m_robotContainer = new RobotContainer();
+    m_leftFront.setIdleMode(IdleMode.kCoast);
+    m_leftRear.setIdleMode(IdleMode.kCoast);
+    m_rightFront.setIdleMode(IdleMode.kCoast);
+    m_rightRear.setIdleMode(IdleMode.kCoast);
+    //m_left.setInverted(true);
+  }
+
+  /**
+   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+   * that you want ran during disabled, autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic() {
+    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
+    // commands, running already-scheduled commands, removing finished or interrupted commands,
+    // and running subsystem periodic() methods.  This must be called from the robot's periodic
+    // block in order for anything in the Command-based framework to work.
+    CommandScheduler.getInstance().run();
+  }
+
+  /** This function is called once each time the robot enters Disabled mode. */
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
+  @Override
+  public void autonomousInit() {
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+
+    // schedule the autonomous command (example)
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.schedule();
+    }
+  }
+
+  /** This function is called periodically during autonomous. */
+  @Override
+  public void autonomousPeriodic() {}
+
+  @Override
+  public void teleopInit() {
+    // This makes sure that the autonomous stops running when
+    // teleop starts running. If you want the autonomous to
+    // continue until interrupted by another command, remove
+    // this line or comment it out.
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.cancel();
+    }
+  }
+
+  /** This function is called periodically during operator control. */
+  @Override
+  public void teleopPeriodic() {
+    SmartDashboard.putNumber("DriveXValue", DriveXValue);
+    DriveXValue = checkDeadband(controller.getLeftX());
+    DriveYValue = checkDeadband(controller.getLeftY());
+    SmartDashboard.putNumber("Controller X", controller.getLeftX());
+    SmartDashboard.putNumber("Controller Y", controller.getLeftY());
+    SmartDashboard.putNumber("DriveXValue", DriveXValue);
+    SmartDashboard.putNumber("DriveYValue", DriveYValue);
+    
+    m_drive.arcadeDrive(DriveXValue, DriveYValue, true);
+  }
+
+  @Override
+  public void testInit() {
+    // Cancels all running commands at the start of test mode.
+    CommandScheduler.getInstance().cancelAll();
+  }
+
+  /** This function is called periodically during test mode. */
+  @Override
+  public void testPeriodic() {}
+
+  /** This function is called once when the robot is first started up. */
+  @Override
+  public void simulationInit() {}
+
+  /** This function is called periodically whilst in simulation. */
+  @Override
+  public void simulationPeriodic() {}
+
+
+  public double checkDeadband(double input)
+  {
+    if(input >= Constants.DEADBAND || input <= -Constants.DEADBAND)
+    {
+      return input;
+    }
+    
+    return 0;
+  }
+}
